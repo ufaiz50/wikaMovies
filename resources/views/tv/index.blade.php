118f8aefeadacd7966959e11aa20768afb23004a
umar faiz <ufaiz50@gmail.com>
2020-05-12 16:26:48 +0700

    commit update again

--- resources/views/tv/index.blade.php
+++ resources/views/tv/index.blade.php
@@ -7,7 +7,7 @@
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
 
                 @foreach ($popularTv as $tvshow)
-                    <x-tv-card :tvshow="$tvshow"/>
+                    <x-tvcard :tvshow="$tvshow"/>
                 @endforeach
 
             </div>
@@ -18,7 +18,7 @@
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
 
                 @foreach ($topRatedTv as $tvshow)
-                    <x-tv-card :tvshow="$tvshow"/>
+                    <x-tvcard :tvshow="$tvshow"/>
                 @endforeach
 
             </div>
