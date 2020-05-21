d42cd45f9609de5a94339b7f0d703cdc56d1e8a5
umar faiz <ufaiz50@gmail.com>
2020-05-21 14:40:06 +0700

    commit update again

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Web update Movies dan Tv show">
+    <meta name="description" 
+    content="Web update Movies dan Tv show berdasarkan themoviedb. 
+    Streaming Movie terupadate full hd, download Movie terupdate full hd">
     <title>Wika Movies</title>
 
 
